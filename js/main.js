fa492095c1a4fd6dcdc600014e8f48f6a95791e9
kimkihong1018 <rhflffk101809@gmail.com>
2024-07-15 17:46:59 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+    //메뉴
+    $(".main > li , .sub_bg").hover(function(){
+        $(".sub, .sub_bg").stop().slideDown();
+    },function(){
+        $(".sub, .sub_bg").stop().slideUp();
+    })
+
+    $(".move").slick({
+        autoplay:true,
+        dots:true,
+        arrows:false,
+        autoplaySpeed:2000,
+        adaptiveHeight:true
+    })
+})//jQ end
